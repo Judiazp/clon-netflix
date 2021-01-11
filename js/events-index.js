207162ebb3c42c9f11823fd4c2bf3d61fb780c4b
jcarlos <judiazp2p@gmail.com>
2021-01-11 17:24:12 -0300

    solucionando un error en js

--- js/events-index.js
+++ js/events-index.js
@@ -5,8 +5,8 @@ const btnPreguntasF = document.querySelectorAll( '.preguntas-frecuentes__list--b
 
 const mostrar = () => {
      for( let i = 0; i < btnPreguntasF.length; i++ ) {    
-         btnPreguntasF[0].addEventListener( 'click', () => {
-             respuesta[0].classList.toggle('preguntas-frecuentes__list--respuesta-open');
+         btnPreguntasF[i].addEventListener( 'click', () => {
+             respuesta[i].classList.toggle('preguntas-frecuentes__list--respuesta-open');
          });
      }
 }
